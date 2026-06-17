41ee2391539089f8b555faeddc48d428a74eaa5d
Bat <extremebat7@gmail.com>
2026-06-17 15:28:36 +0300

    Outline upd

--- Scripts/Interacting.cs
+++ Scripts/Interacting.cs
@@ -23,7 +23,14 @@ public class Interacting : MonoBehaviour
     [Header("Visual")]
     [SerializeField] Material outline;
 
-    HashSet<Renderer> outlined = new HashSet<Renderer>();
+    [Header("3D Outline")]
+    HashSet<Renderer> outlined3D = new();
+    Dictionary<GameObject, List<GameObject>> outlined2D = new();
+
+    [Header("2D Outline")]
+    [SerializeField] float outlineScale = 0.05f;
+    [SerializeField] Color outlineColor = Color.white;
+
 
     [Header("Locking")]
     public GameObject localLock;
@@ -124,28 +131,130 @@ public class Interacting : MonoBehaviour
     // Outline
     void SetOutline(GameObject obj)
     {
-        if (obj == null) return;
-        var rend = obj.GetComponent<Renderer>();
-        if (!rend || outlined.Contains(rend)) return;
+        switch (interactionMode)
+        {
+            case InteractionMode.Mode2D:
+                SetOutline2D(obj);
+                break;
 
-        var mats = new List<Material>(rend.materials);
-        mats.Add(outline);
-        rend.materials = mats.ToArray();
-
-        outlined.Add(rend);
+            case InteractionMode.Mode3D:
+                SetOutline3D(obj);
+                break;
+        }
     }
 
     void RemoveOutline(GameObject obj)
     {
+        switch (interactionMode)
+        {
+            case InteractionMode.Mode2D:
+                RemoveOutline2D(obj);
+                break;
+
+            case InteractionMode.Mode3D:
+                RemoveOutline3D(obj);
+                break;
+        }
+    }
+
+    // 3D
+    void SetOutline3D(GameObject obj)
+    {
         if (obj == null) return;
-        var rend = obj.GetComponent<Renderer>();
-        if (!rend) return;
 
-        var mats = new List<Material>(rend.materials);
-        mats.RemoveAll(m => m.shader == outline.shader);
-        rend.materials = mats.ToArray();
+        foreach (Renderer rend in obj.GetComponentsInChildren<Renderer>())
+        {
+            if (outlined3D.Contains(rend))
+                continue;
 
-        outlined.Remove(rend);
+            List<Material> mats = new(rend.materials);
+            mats.Add(outline);
+
+            rend.materials = mats.ToArray();
+
+            outlined3D.Add(rend);
+        }
+    }
+
+    void RemoveOutline3D(GameObject obj)
+    {
+        if (obj == null) return;
+
+        foreach (Renderer rend in obj.GetComponentsInChildren<Renderer>())
+        {
+            List<Material> mats = new(rend.materials);
+
+            mats.RemoveAll(m => m.shader == outline.shader);
+
+            rend.materials = mats.ToArray();
+
+            outlined3D.Remove(rend);
+        }
+    }
+
+    // 2D
+    void SetOutline2D(GameObject obj)
+    {
+        if (obj == null)
+            return;
+
+        if (outlined2D.ContainsKey(obj))
+            return;
+
+        SpriteRenderer source = obj.GetComponent<SpriteRenderer>();
+
+        if (source == null)
+            return;
+
+        List<GameObject> clones = new();
+
+        GameObject outer = new("Outline Outer");
+        GameObject inner = new("Outline Inner");
+
+        outer.transform.SetParent(obj.transform, false);
+        inner.transform.SetParent(obj.transform, false);
+
+        SpriteRenderer outerSR = outer.AddComponent<SpriteRenderer>();
+        SpriteRenderer innerSR = inner.AddComponent<SpriteRenderer>();
+
+        outerSR.sprite = source.sprite;
+        innerSR.sprite = source.sprite;
+
+        outerSR.color = outlineColor;
+        innerSR.color = outlineColor;
+
+        outerSR.material = outline;
+        innerSR.material = outline;
+
+        outerSR.sortingLayerID = source.sortingLayerID;
+        innerSR.sortingLayerID = source.sortingLayerID;
+
+        outerSR.sortingOrder = source.sortingOrder - 1;
+        innerSR.sortingOrder = source.sortingOrder - 1;
+
+        outer.transform.localScale =
+            Vector3.one * (1f + outlineScale);
+
+        inner.transform.localScale =
+            Vector3.one * (1f - outlineScale);
+
+        clones.Add(outer);
+        clones.Add(inner);
+
+        outlined2D.Add(obj, clones);
+    }
+    void RemoveOutline2D(GameObject obj)
+    {
+        if (!outlined2D.TryGetValue(obj, out List<GameObject> clones))
+            return;
+
+        foreach (GameObject clone in clones)
+        {
+            if (clone != null)
+                Destroy(clone);
+        }
+
+        outlined2D.Remove(obj);
     }
 
     // Hover
